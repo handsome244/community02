6e24ea869ee6789dc6e9ab742ffbddf46a832e5e
Admin <1468350571@qq.com>
2020-06-21 15:49:44 +0800

    添加bootstrap

---
+++ src/main/java/life/majiang/community/controller/HelloController.java
@@ -0,0 +1,20 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Description
+ * @auther
+ * @date
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
